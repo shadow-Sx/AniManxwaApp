5dd89ccbfba50fa235622dbca77508c5662633bb
shadow-Sx <shadowsoya8@gmail.com>
2026-04-05 23:01:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,96 +1,74 @@
 import os
 import json
-import base64
-import urllib.parse
-import io
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
 from dotenv import load_dotenv
-import requests
+from mega import Mega
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-# Mega.nz API (public link yaratish uchun)
-MEGA_EMAIL = os.getenv('MEGA_EMAIL')
-MEGA_PASSWORD = os.getenv('MEGA_PASSWORD')
+# Mega.nz ga ulanish
+mega = Mega()
+m = mega.login(os.getenv('MEGA_EMAIL'), os.getenv('MEGA_PASSWORD'))
 
-# Ma'lumotlar bazasi
-MANGA_DB = {
-    'solo-leveling': {
-        'title': 'Solo Leveling',
-        'author': 'Chugong',
-        'type': 'bepul',
-        'cover': 'https://mega.nz/embed/uK5mXYAK#aLtTAa30aJUxtYcB5EszmB2pk3i_rR2yVaHQRjfrD_s',
-        'chapters': 179
-    }
-}
+def get_manga_list():
+    """Mega.nz dan mangalar ro'yxatini olish"""
+    mangas = []
+    mangas_folder = m.find('Mangas')
+    
+    if mangas_folder:
+        for folder in mangas_folder.children:
+            if folder.is_dir:
+                info_file = folder.find('info.json')
+                if info_file:
+                    info = json.loads(m.download(info_file))
+                    mangas.append({
+                        'id': folder.name,
+                        'title': info['title'],
+                        'author': info['author'],
+                        'type': info['type'],
+                        'cover': f'/cover/{folder.name}',
+                        'chapters': len([f for f in folder.children if f.is_dir and f.name.startswith('bob_')])
+                    })
+    return mangas
 
 @app.route('/mangas', methods=['GET'])
 def get_mangas():
-    """Barcha mangalar ro'yxatini olish"""
-    mangas = []
-    for manga_id, data in MANGA_DB.items():
-        mangas.append({
-            'id': manga_id,
-            'title': data['title'],
-            'author': data['author'],
-            'type': data['type'],
-            'cover': data['cover'],
-            'chapters': data['chapters']
-        })
-    return jsonify(mangas)
+    return jsonify(get_manga_list())
 
-@app.route('/image/<path:url>', methods=['GET'])
-def get_image(url):
-    """Mega.nz dan rasmni qaytarish"""
-    try:
-        decoded_url = urllib.parse.unquote(url)
-        response = requests.get(decoded_url)
-        return send_file(
-            io.BytesIO(response.content),
-            mimetype='image/png'
-        )
-    except Exception as e:
-        return jsonify({'error': str(e)}), 404
+@app.route('/cover/<manga_id>', methods=['GET'])
+def get_cover(manga_id):
+    """Muqovani olish"""
+    manga = m.find(f'Mangas/{manga_id}')
+    cover = manga.find('cover.jpg') or manga.find('cover.png')
+    if cover:
+        return send_file(m.download(cover), mimetype='image/jpeg')
+    return '', 404
 
-@app.route('/upload', methods=['POST'])
-def upload_manga():
-    """Yangi manga yuklash"""
-    try:
-        data = request.json
-        manga_id = data['manga_id']
-        
-        MANGA_DB[manga_id] = {
-            'title': data['title'],
-            'author': data['author'],
-            'type': data['type'],
-            'cover': data.get('cover', 'https://via.placeholder.com/200x300'),
-            'chapters': len(data.get('chapters', []))
-        }
-        
-        return jsonify({'success': True, 'message': 'Manga yuklandi!', 'manga_id': manga_id})
-    except Exception as e:
-        return jsonify({'success': False, 'error': str(e)}), 500
+@app.route('/page/<manga_id>/<path:page_path>', methods=['GET'])
+def get_page(manga_id, page_path):
+    """Sahifani olish (bob/rasm)"""
+    full_path = f'Mangas/{manga_id}/{page_path}'
+    file = m.find(full_path)
+    if file:
+        return send_file(m.download(file), mimetype='image/png')
+    return '', 404
 
 @app.route('/chapters/<manga_id>', methods=['GET'])
 def get_chapters(manga_id):
-    """Manga boblari ro'yxatini olish"""
-    if manga_id in MANGA_DB:
-        chapters = []
-        for i in range(1, MANGA_DB[manga_id]['chapters'] + 1):
+    """Boblar ro'yxatini olish"""
+    manga = m.find(f'Mangas/{manga_id}')
+    chapters = []
+    for folder in manga.children:
+        if folder.is_dir and folder.name.startswith('bob_'):
             chapters.append({
-                'number': i,
-                'pages': 30  # Test uchun
+                'number': int(folder.name.split('_')[1]),
+                'pages': len(folder.children)
             })
-        return jsonify(chapters)
-    return jsonify([])
-
-@app.route('/health', methods=['GET'])
-def health():
-    return jsonify({'status': 'ok'})
+    return jsonify(sorted(chapters, key=lambda x: x['number']))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
