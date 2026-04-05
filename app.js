445399389c494cdc99446b19113a19d040162eb2
shadow-Sx <shadowsoya8@gmail.com>
2026-04-05 23:52:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { Mega } = require('megajs');
+const Mega = require('megajs');
 const multer = require('multer');
 require('dotenv').config();
 
